8890381e2c0fb4b41416a1cc50d5edf879e75e5d
Alex Arbogast <arbogastaw@gmail.com>
2025-03-08 12:55:58 -0500

    add "find symbols" nvim mapping

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,3 +8,6 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 map("i", "<C-BS>", "<ESC>cvb")
+
+-- telescope
+map("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>", { desc = "telescope lsp document symbols" })
