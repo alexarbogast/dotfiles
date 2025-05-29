22e438695351948df422678824842a313a03bf48
Alex Arbogast <arbogastaw@gmail.com>
2025-05-29 11:39:45 -0400

    add vim-tmux-navigator plugin

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,7 +1,5 @@
 require("nvchad.mappings")
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
@@ -10,8 +8,15 @@ map("i", "jk", "<ESC>")
 map("i", "<C-BS>", "<ESC>cvb")
 
 -- telescope
-map("n", "<leader>fs", require('telescope.builtin').lsp_document_symbols, { desc = "Telescope lsp document symbols" })
-map("n", "<leader>fr", require('telescope.builtin').lsp_references, { desc = "Telescope lsp references" })
+map("n", "<leader>fs", require("telescope.builtin").lsp_document_symbols, { desc = "Telescope lsp document symbols" })
+map("n", "<leader>fr", require("telescope.builtin").lsp_references, { desc = "Telescope lsp references" })
+
+-- tmux navigation (fixing conflicting nvchad + vim-tmux-navtigator mappings)
+map("n", "<c-h>", "<cmd>:TmuxNavigateLeft<cr>")
+map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>")
+map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>")
+map("n", "<c-l>", "<cmd>:TmuxNavigateRight<cr>")
+map("n", "<c-\\>", "<cmd>:TmuxNavigatePrevious<cr>")
 
 -- lsp
 map("n", "<leader>df", function()
