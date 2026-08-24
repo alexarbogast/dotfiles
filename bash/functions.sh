6f614b4ca695e57018ffc17e66e6c3b971099870
Alex Arbogast <arbogastaw@gmail.com>
2026-08-23 23:25:14 -0400

    feat: add local_aliases function
    This allows you to edit or create the local aliases file with one command

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,3 +1,21 @@
+local_aliases() {
+  local file="$DOTFILES_ROOT/bash/local_aliases.sh"
+
+  if [[ ! -f "$file" ]]; then
+    mkdir -p "$(dirname "$file")"
+
+    cat > "$file" <<'EOF'
+# This script provides a method for sourcing local aliases that are not tracked
+# by version control
+
+# =========== Add local (machine dependent) aliases below ===========
+
+EOF
+  fi
+
+  nvim "$file"
+}
+
 nvim-reset() {
     echo "WARNING: This will remove plugins and data."
     read -p "Continue? [y/N] " confirm
